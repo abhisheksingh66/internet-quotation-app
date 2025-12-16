c9ad7a0ad03925ce0cb413e1e0ffc1045ee6ae05
Abhishek Singh <124181034+abhisheksingh66@users.noreply.github.com>
2025-12-16 22:12:00 +0530

    Update PackageController.java

--- Backend/src/main/java/com/backend/controller/PackageController.java
+++ Backend/src/main/java/com/backend/controller/PackageController.java
@@ -1,25 +1,29 @@
 package com.backend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import model.InternetPackage;
+import com.backend.model.InternetPackage;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/packages")
+    
 @CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/packages")
 public class PackageController {
 
     @GetMapping
     public List<InternetPackage> getPackages() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        return List.of(
-                mapper.readValue(
-                        new File("src/main/resources/packages.json"),
-                        InternetPackage[].class
-                )
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        File file = new File("src/main/resources/packages.json");
+
+        InternetPackage[] packages = objectMapper.readValue(
+                file,
+                InternetPackage[].class
         );
+
+        return List.of(packages);
     }
 }
