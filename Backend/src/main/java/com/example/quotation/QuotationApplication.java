bef05980a8b12ea6f502ff2c02bcffeba706c518
Abhishek Singh <124181034+abhisheksingh66@users.noreply.github.com>
2025-12-16 22:13:31 +0530

    Update QuotationApplication.java

--- Backend/src/main/java/com/example/quotation/QuotationApplication.java
+++ Backend/src/main/java/com/example/quotation/QuotationApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuotationApplication {
     public static void main(String[] args) {
+        System..out.println("hello jva");
         SpringApplication.run(QuotationApplication.class, args);
     }
 }
